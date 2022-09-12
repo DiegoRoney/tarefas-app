4a42e586a342dd6c608d5adb18d1be319c0fb07f
Diego R <diego-roney@outlook.com>
2022-09-12 19:50:00 -0300

    up

---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+export default function Form(){
+    return( 
+        <div> 
+             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+    )
+       
+ };
